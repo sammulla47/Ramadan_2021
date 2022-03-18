403b43b3b2c789abf39a197334bf6e1436c61694
Sameer Mulla <sammulla46@gmail.com>
2022-03-18 20:25:36 +0530

    Add files via upload

---
+++ Ramadan_2021/ramadan 2022/asset/elements.js
@@ -0,0 +1,193 @@
+class Elements {
+	static GetHeadings() {
+		return ["Ramadan", "Days/Day", "Sehar", "Iftar"];
+	}
+	static GetProperties() {
+		return ["ramadan", "dateday", "sehar", "iftar"];
+	}
+	static GetElements() {
+		return [
+			{
+				ramadan: 1,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 2,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 3,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 4,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 5,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 6,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 7,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 8,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 9,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 10,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 11,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 12,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+
+				ramadan: 13,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 14,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 15,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 16,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 17,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 18,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 19,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 20,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 21,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 22,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 23,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 24,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 25,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 26,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 27,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 28,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 29,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			},
+			{
+				ramadan: 30,
+				dateday: "12/03/12",
+				sehar: "H",
+				iftar: "5:30"
+			}
+		]
+	}
+}
